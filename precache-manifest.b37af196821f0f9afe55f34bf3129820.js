40069298542f7a658b23f4ae6a913291dab31900
Alin Simoc <alin.s@softisfy.com>
2021-12-02 14:40:15 +0200

    Updates

---
+++ precache-manifest.b37af196821f0f9afe55f34bf3129820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ac44f774b9c8d24e556e0a510be63f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eab4fe822fd7debd90ca",
+    "url": "./static/css/main.19985397.chunk.css"
+  },
+  {
+    "revision": "cef351bdfde8785b1233",
+    "url": "./static/js/2.e285a9fa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e285a9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eab4fe822fd7debd90ca",
+    "url": "./static/js/main.3b47375d.chunk.js"
+  },
+  {
+    "revision": "09f820f6cf3741453d9a",
+    "url": "./static/js/runtime-main.dde9e534.js"
+  }
+]);
